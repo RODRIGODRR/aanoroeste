c6a7751dee061b4e2c6c035e80b80891068ac95d
Rodrigo Silva <rodridrr_04@hotmail.com>
2019-12-30 14:03:24 -0300

    -pequenos ajustes em css

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,3 +2,7 @@ $(document).ready(function () {
     $('#header').load('masters/header.html');
     $('#footer').load('masters/footer.html');            
 });
+
+scrollDown = () => {
+    $("html, body").animate({ scrollTop: "50px" });
+}

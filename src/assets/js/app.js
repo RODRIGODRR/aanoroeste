f766211ef3a0697855982d45b6fd17e91fcb1cf4
Rodrigo Silva <rodridrr_04@hotmail.com>
2020-05-24 18:47:59 -0300

    -alguns ajustes gerais + inclusão de fotos (ainda não estão sendo consumidas)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,3 +6,22 @@ $(document).ready(function () {
 scrollDown = () => {
     $("html, body").animate({ scrollTop: "50px" });
 }
+
+// var imgArray = [
+//     'assets/img/banner_bg.png',
+//     'assets/img/banner_bg2.png',
+//     'assets/img/banner_bg3.png'],
+//     curIndex = 0;
+//     imgDuration = 3000;
+    
+//     function slideShow() {
+//         $('.banner_part').addClass('fadeOut');
+//         setTimeout(function() {            
+//             $('.banner_part').css("background-image", "url('"+ imgArray[curIndex] +"')");
+//             $('.banner_part').removeClass('fadeOut');
+//         },1000);
+//         curIndex++;
+//         if (curIndex == imgArray.length) { curIndex = 0; }
+//         setTimeout(slideShow, imgDuration);
+//     }
+//     slideShow();
